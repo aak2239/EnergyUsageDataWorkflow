70574392868a6fb7f9a824bce51284c187ae3d44
Abd.Kahhaleh <139154725+aak2239@users.noreply.github.com>
2023-11-30 15:27:15 -0500

    Update process_delivery_dates.py

--- process_delivery_dates.py
+++ process_delivery_dates.py
@@ -15,12 +15,26 @@ def process_delivery_dates(file_path, intermediate_dir_path):
     df['End Date'] = pd.NaT
 
     for i in df.index:
-        # ... [Rest of the function remains the same] ...
-
+        current_delivery = df.at[i, 'Delivery Date']
+        if pd.notnull(current_delivery):
+            df.at[i, 'Start Date'] = current_delivery
+            if i < df.index[-1]:
+                next_delivery = df.at[i + 1, 'Delivery Date']
+                if pd.notnull(next_delivery) and (next_delivery - current_delivery).days <= 30:
+                    df.at[i, 'End Date'] = next_delivery - timedelta(days=1)
+                else:
+                    df.at[i, 'End Date'] = current_delivery + timedelta(days=30)
+                    df.at[i, 'Comment'] = 'Fuel usage cycle end date assumed as 30 days within the delivery date.'
+            else:
+                df.at[i, 'End Date'] = current_delivery + timedelta(days=30)
+                df.at[i, 'Comment'] = 'Fuel usage cycle end date assumed as 30 days within the delivery date.'
+    
     df['Start Date'] = df['Start Date'].dt.strftime('%m/%d/%y')
     df['End Date'] = df['End Date'].dt.strftime('%m/%d/%y')
+
     df['Start Date'].replace({pd.NaT: ''}, inplace=True)
     df['End Date'].replace({pd.NaT: ''}, inplace=True)
+
     df.drop(columns=['Delivery Date'], inplace=True)
 
     intermediate_file_path = intermediate_dir_path / f"{file_path.stem}_intermediate.xlsx"
